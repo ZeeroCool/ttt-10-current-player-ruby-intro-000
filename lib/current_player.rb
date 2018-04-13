505e4392cc2f7ecdffe29140b288f1392ff4fbc8
David Z <davidzabran@gmail.com>
2018-04-13 15:26:47 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,23 @@
+
+#turn_count Method counts how many turns have been taken in the current game
+def turn_count(board)
+
+  counter = 0
+
+  board.each do |turn|
+    if turn != " "
+    counter += 1
+    end
+  end
+  puts counter
+end
+
+#current_player method determins which players turn it is
+  def current_player(board)
+
+    if turn_count(board).even? == true
+      puts "X"
+    else
+      puts "O"
+    end
+  end
